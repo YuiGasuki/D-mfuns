caedda53b83497f57d08a67253275fb94bd0f9ed
Yui <140684321+YuiGasuki@users.noreply.github.com>
2025-07-31 12:21:20 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -7,23 +7,19 @@ let videoNow
 let downloadClarity = [];
 
 chrome.tabs.query({ currentWindow: true, active: true }).then(tabs => {
-        if (tabs[0].url.search(".mfuns.net/video/") !== -1) {
-            getDataUrl()
-        } else {
-            document.body.innerHTML = `<p class='warn'>这不是我想要的页面(っ °Д °;)っ</p>`
-        }
-    });
+    if (tabs[0].url.search(".mfuns.net/video/") !== -1) {
+        getDataUrl()
+    } else {
+        document.body.innerHTML = `<p class='warn'>这不是我想要的页面(っ °Д °;)っ</p>`
+    }
+});
 
 function getDataUrl() {
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, (tabs) => {
-        let timeoutId = setTimeout(() => {
-            document.body.innerHTML = `<p class='warn'>少女祈祷中</p>`
-        }, 300)
-        chrome.tabs.sendMessage(tabs[0].id, 'hi', res => {
-            clearTimeout(timeoutId)
+        chrome.tabs.sendMessage(tabs[0].id, 'hi').then(res => {
             if (res === undefined) {
                 document.body.innerHTML = `<p class='warn'>页面还没有加载完成(っ °Д °;)っ</p>`
             } else {
@@ -45,21 +41,32 @@ function getDataUrl() {
                 videoList.id = 'videoList';
                 document.body.appendChild(videoList)
                 getVideoList(videoList, div);
+
+                const buttonBox = document.createElement('div');
                 const downButton = document.createElement('button');
                 downButton.id = 'downButton';
                 downButton.innerText = '下载选择'
-                downButton.disabled = true
                 downButton.className = 'disabled'
                 downButton.onclick = () => getDownloadUrl()
-                document.body.appendChild(downButton)
+                buttonBox.appendChild(downButton)
                 const cancelButton = document.createElement('button');
                 cancelButton.id = 'cancelButton';
                 cancelButton.innerText = '取消选择'
-                cancelButton.disabled = true
                 cancelButton.className = "disabled"
                 cancelButton.onclick = () => clickCancel()
-                document.body.appendChild(cancelButton)
+                buttonBox.appendChild(cancelButton)
+                const getallButton = document.createElement('button');
+                getallButton.id = 'getallButton';
+                getallButton.innerText = '全部选择'
+                getallButton.className = "disabledFalse"
+                getallButton.onclick = () => getAll()
+                buttonBox.appendChild(getallButton)
+                buttonBox.id = 'buttonBox'
+                document.body.appendChild(buttonBox)
+                videoList.style.marginBottom = `${buttonBox.offsetHeight + 8}px`
             }
+        }).catch(error => {
+            document.body.innerHTML = `<p class='warn'>少女祈祷中...</p>`
         })
 
         function getDownloadUrl() {
@@ -90,7 +97,7 @@ function getClarityList(el, id) {
         clarityData.innerText = `${data.videoUrl[id].list[i].name} ${data.videoUrl[id].list[i].label}`
         clarityData.onclick = () => {
             downloadClarity[videoNow] = i;
-            document.getElementById('video' + videoNow).innerHTML = (data.videoUrl[videoNow].title.length > 20 ? data.videoUrl[videoNow].title.substring(0,19)+'...' : data.videoUrl[videoNow].title) + ' ' + `${data.videoUrl[videoNow].list[i].name} ${data.videoUrl[videoNow].list[i].label} <span>${getSize(data.videoUrl[videoNow].list[i].size)}</span>`;
+            document.getElementById('video' + videoNow).innerHTML = (data.videoUrl[videoNow].title.length > 20 ? data.videoUrl[videoNow].title.substring(0, 19) + '...' : data.videoUrl[videoNow].title) + ' ' + `${data.videoUrl[videoNow].list[i].name} ${data.videoUrl[videoNow].list[i].label}<br><span>${getSize(data.videoUrl[videoNow].list[i].size)}</span>`;
             Array.from(el.children).forEach(element => {
                 element.style.color = "#aaaaaa"
             });
@@ -106,14 +113,12 @@ function getVideoList(el, clarityList) {
     for (let i = 0; i < data.videoUrl.length; i++) {
         const videoData = document.createElement('div');
         videoData.className = 'videoButton'
-        videoData.innerHTML = (data.videoUrl[i].title.length > 20 ? data.videoUrl[i].title.substring(0,19)+'...' : data.videoUrl[i].title) + ' ' + `${data.videoUrl[i].list[data.videoUrl[i].list.length - 1].name} ${data.videoUrl[i].list[data.videoUrl[i].list.length - 1].label} <span>${getSize(data.videoUrl[i].list[data.videoUrl[i].list.length - 1].size)}</span>`;
+        videoData.innerHTML = (data.videoUrl[i].title.length > 20 ? data.videoUrl[i].title.substring(0, 19) + '...' : data.videoUrl[i].title) + ' ' + `${data.videoUrl[i].list[data.videoUrl[i].list.length - 1].name} ${data.videoUrl[i].list[data.videoUrl[i].list.length - 1].label}<br><span>${getSize(data.videoUrl[i].list[data.videoUrl[i].list.length - 1].size)}</span>`;
         downloadUrl.push(0)
         downloadClarity.push(data.videoUrl[i].list.length - 1)
         videoData.id = 'video' + i
         videoData.onclick = () => {
-            cancelButton.disabled = false
             cancelButton.className = "disabledFalse"
-            downButton.disabled = false
             downButton.className = 'disabledFalse'
             downloadUrl[i] = 1;
             videoNow = i;
@@ -123,11 +128,41 @@ function getVideoList(el, clarityList) {
             });
             videoData.style.borderColor = '#A78BFA'
             videoData.style.color = '#A78BFA'
+            for (let i = 0; i < downloadUrl.length; i++) {
+                if (downloadUrl[i] === 0) {
+                    return
+                }
+            }
+            getallButton.className = 'disabledFalse'
+            getallButton.innerText = "全部取消"
+            getallButton.onclick = () => getAllCancel(getallButton)
         }
         el.appendChild(videoData)
     }
 }
 
+function getAll() {
+    for (let i = 0; i < data.videoUrl.length; i++) {
+        document.getElementById('video' + i).onclick();
+    }
+    getallButton.innerText = "全部取消"
+    getallButton.onclick = () => getAllCancel(getallButton)
+}
+
+function getAllCancel(el) {
+    for (let i = 0; i < downloadUrl.length; i++) {
+        downloadUrl[i] = 0
+        const videoData = document.getElementById('video' + i)
+        videoData.style.borderColor = '#aaaaaa'
+        videoData.style.color = '#aaaaaa'
+        videoNow = null;
+    }
+    document.getElementById('clarityList').innerHTML = `<div class='clarityButton'>无</div>`
+    cancelButton.className = "disabled"
+    downButton.className = 'disabled'
+    el.innerText = "全部选择"
+    el.onclick = () => getAll()
+}
 
 
 function clickCancel() {
@@ -139,21 +174,22 @@ function clickCancel() {
     videoNow = null;
     for (let i = 0; i < downloadUrl.length; i++) {
         if (downloadUrl[i] === 1) {
-            cancelButton.disabled = true
             cancelButton.className = 'disabled'
+            getallButton.className = 'disabled'
             return
         }
     }
-    cancelButton.disabled = true
     cancelButton.className = "disabled"
-    downButton.disabled = true
     downButton.className = 'disabled'
+    getallButton.className = 'disabledFalse'
+    getallButton.innerText = "全部选择"
+    getallButton.onclick = () => getAll()
 }
 
-function getSize(size){
-    size=size*1
-    if(size>=1024*1024*1024) return (size/(1024*1024*1024)).toFixed(2) + 'G'
-    if(size>=1024*1024) return (size/(1024*1024)).toFixed(2) + 'MB'
-    if(size>=1024) return (size/1024).toFixed(2) + 'KB'
+function getSize(size) {
+    size = size * 1
+    if (size >= 1024 * 1024 * 1024) return (size / (1024 * 1024 * 1024)).toFixed(2) + 'G'
+    if (size >= 1024 * 1024) return (size / (1024 * 1024)).toFixed(2) + 'MB'
+    if (size >= 1024) return (size / 1024).toFixed(2) + 'KB'
     return size + 'B'
 }
